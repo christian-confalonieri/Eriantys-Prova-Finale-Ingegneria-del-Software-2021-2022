72d734c4d579611d205bdc4e70b567d732515df0
Christian Confalonieri <100704012+christian-confalonieri@users.noreply.github.com>
2022-04-06 09:47:46 +0200

    Implemented Thief

--- src/main/java/it/polimi/ingsw/model/power/Thief.java
+++ src/main/java/it/polimi/ingsw/model/power/Thief.java
@@ -1,7 +1,13 @@
 package it.polimi.ingsw.model.power;
 
+import it.polimi.ingsw.model.entity.Player;
+import it.polimi.ingsw.model.entity.Student;
+import it.polimi.ingsw.model.enumeration.PawnColor;
 import it.polimi.ingsw.model.enumeration.PowerType;
 import it.polimi.ingsw.model.game.GameHandler;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Thief extends PowerCard {
 
@@ -14,11 +20,30 @@ public class Thief extends PowerCard {
         setCost(3);
     }
 
+    /**
+     * @author Christian Confalonieri
+     */
     @Override
     public void power() {
         super.power();
 
-        //TODO
+        List<Player> players = getGameHandler().getOrderedTurnPlayers();
+        PawnColor color = getGameHandler().getGame().getEffectHandler().getThiefColor();
+        List<Student> studentsBag = new ArrayList<>();
+        List<Student> studentsDiningRoom;
+
+        for(Player player : players) {
+            studentsBag.clear();
+            studentsDiningRoom = player.getSchool().getStudentsDiningRoom(color);
+            for(int i=0;i<3;i++) {
+                if(!studentsDiningRoom.isEmpty()) {
+                    studentsBag.add(studentsDiningRoom.get(0));
+                    studentsDiningRoom.remove(0);
+                }
+
+            }
+            getGameHandler().getGame().getBag().addAllStudents(studentsBag);
+        }
     }
 
 }
