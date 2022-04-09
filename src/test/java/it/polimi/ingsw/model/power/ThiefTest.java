136ce71e2730cf71c84ef2c6958fa127d2cf2bfb
Christian Confalonieri <100704012+christian-confalonieri@users.noreply.github.com>
2022-04-09 16:38:18 +0200

    Thief test

---
+++ src/test/java/it/polimi/ingsw/model/power/ThiefTest.java
@@ -0,0 +1,131 @@
+package it.polimi.ingsw.model.power;
+
+import it.polimi.ingsw.exceptions.InvalidNewGameException;
+import it.polimi.ingsw.exceptions.InvalidRulesException;
+import it.polimi.ingsw.model.entity.Player;
+import it.polimi.ingsw.model.entity.Professor;
+import it.polimi.ingsw.model.entity.Student;
+import it.polimi.ingsw.model.enumeration.PawnColor;
+import it.polimi.ingsw.model.enumeration.PowerType;
+import it.polimi.ingsw.model.enumeration.Wizard;
+import it.polimi.ingsw.model.game.GameCreator;
+import it.polimi.ingsw.model.game.GameHandler;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.SortedMap;
+import java.util.TreeMap;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @author Christian Confalonieri
+ */
+class ThiefTest {
+
+    /**
+     * @author Christian Confalonieri
+     */
+    @Test
+    void power() throws InvalidNewGameException, IOException, InvalidRulesException {
+        GameHandler gameHandler = createGame();
+
+        Thief thief = new Thief(gameHandler);
+        List<PowerCard> powerCards = new ArrayList<>();
+        powerCards.add(thief);
+        gameHandler.getGame().setPowerCards(powerCards);
+
+        assertEquals(PowerType.THIEF,thief.getType());
+        assertEquals(3,thief.getCost());
+
+        List<Player> players = gameHandler.getOrderedTurnPlayers();
+        // 0: Luigi, 1: Mario
+        Professor redProfessor = gameHandler.getGame().getProfessor(PawnColor.RED);;
+        Professor greenProfessor = gameHandler.getGame().getProfessor(PawnColor.GREEN);;
+        Professor yellowProfessor = gameHandler.getGame().getProfessor(PawnColor.YELLOW);;
+
+        players.get(1).getSchool().addProfessor(redProfessor);
+        gameHandler.getGame().removeProfessor(redProfessor);
+        players.get(0).getSchool().addProfessor(greenProfessor);
+        gameHandler.getGame().removeProfessor(greenProfessor);
+        players.get(0).getSchool().addProfessor(yellowProfessor);
+        gameHandler.getGame().removeProfessor(yellowProfessor);
+
+        // add students in the Luigi's dining room
+        for(int i=0; i<3; i++) {
+            players.get(0).getSchool().addDiningRoom(new Student(PawnColor.RED));
+        }
+        for(int i=0; i<3; i++) {
+            players.get(0).getSchool().addDiningRoom(new Student(PawnColor.GREEN));
+        }
+        for(int i=0; i<0; i++) {
+            players.get(0).getSchool().addDiningRoom(new Student(PawnColor.BLUE));
+        }
+        for(int i=0; i<4; i++) {
+            players.get(0).getSchool().addDiningRoom(new Student(PawnColor.YELLOW));
+        }
+        for(int i=0; i<0; i++) {
+            players.get(0).getSchool().addDiningRoom(new Student(PawnColor.PINK));
+        }
+
+        // add students in the Mario's dining room
+        for(int i=0; i<6; i++) {
+            players.get(1).getSchool().addDiningRoom(new Student(PawnColor.RED));
+        }
+        for(int i=0; i<3; i++) {
+            players.get(1).getSchool().addDiningRoom(new Student(PawnColor.GREEN));
+        }
+        for(int i=0; i<0; i++) {
+            players.get(1).getSchool().addDiningRoom(new Student(PawnColor.BLUE));
+        }
+        for(int i=0; i<2; i++) {
+            players.get(1).getSchool().addDiningRoom(new Student(PawnColor.YELLOW));
+        }
+        for(int i=0; i<0; i++) {
+            players.get(1).getSchool().addDiningRoom(new Student(PawnColor.PINK));
+        }
+
+        assertEquals(1,players.get(1).getSchool().getProfessorTable().size());
+        assertEquals(2,players.get(0).getSchool().getProfessorTable().size());
+        assertEquals(redProfessor,players.get(1).getSchool().getProfessorTable().get(0));
+
+        gameHandler.getGame().getEffectHandler().setThiefColor(PawnColor.YELLOW);
+        assertEquals(3,players.get(0).getSchool().getStudentsDiningRoom(PawnColor.RED).size());
+        assertEquals(3,players.get(0).getSchool().getStudentsDiningRoom(PawnColor.GREEN).size());
+        assertEquals(0,players.get(0).getSchool().getStudentsDiningRoom(PawnColor.BLUE).size());
+        assertEquals(4,players.get(0).getSchool().getStudentsDiningRoom(PawnColor.YELLOW).size());
+        assertEquals(0,players.get(0).getSchool().getStudentsDiningRoom(PawnColor.PINK).size());
+        assertEquals(6,players.get(1).getSchool().getStudentsDiningRoom(PawnColor.RED).size());
+        assertEquals(3,players.get(1).getSchool().getStudentsDiningRoom(PawnColor.GREEN).size());
+        assertEquals(0,players.get(1).getSchool().getStudentsDiningRoom(PawnColor.BLUE).size());
+        assertEquals(2,players.get(1).getSchool().getStudentsDiningRoom(PawnColor.YELLOW).size());
+        assertEquals(0,players.get(1).getSchool().getStudentsDiningRoom(PawnColor.PINK).size());
+
+        thief.power();
+
+        assertEquals(3,players.get(0).getSchool().getStudentsDiningRoom(PawnColor.RED).size());
+        assertEquals(3,players.get(0).getSchool().getStudentsDiningRoom(PawnColor.GREEN).size());
+        assertEquals(0,players.get(0).getSchool().getStudentsDiningRoom(PawnColor.BLUE).size());
+        assertEquals(1,players.get(0).getSchool().getStudentsDiningRoom(PawnColor.YELLOW).size());
+        assertEquals(0,players.get(0).getSchool().getStudentsDiningRoom(PawnColor.PINK).size());
+        assertEquals(6,players.get(1).getSchool().getStudentsDiningRoom(PawnColor.RED).size());
+        assertEquals(3,players.get(1).getSchool().getStudentsDiningRoom(PawnColor.GREEN).size());
+        assertEquals(0,players.get(1).getSchool().getStudentsDiningRoom(PawnColor.BLUE).size());
+        assertEquals(0,players.get(1).getSchool().getStudentsDiningRoom(PawnColor.YELLOW).size());
+        assertEquals(0,players.get(1).getSchool().getStudentsDiningRoom(PawnColor.PINK).size());
+    }
+
+    private GameHandler createGame() throws IOException, InvalidNewGameException, InvalidRulesException {
+        SortedMap<String, Wizard> playerData = new TreeMap<>();
+        playerData.put("Mario", Wizard.PURPLE);
+        playerData.put("Luigi",Wizard.GREEN);
+        String rulesJson;
+        rulesJson = new String(Files.readAllBytes(Paths.get("src/main/resources/Rules2P.json")));
+        GameHandler gameHandler = GameCreator.createGame(playerData, rulesJson);
+        return gameHandler;
+    }
+}
