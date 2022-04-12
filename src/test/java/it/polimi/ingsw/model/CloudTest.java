e2ac1d8f9f52d7f8071dbc7da3ea33a7acdf7bbb
christian-confalonieri <100704012+christian-confalonieri@users.noreply.github.com>
2022-04-12 12:26:46 +0200

    Cloud Test

--- src/test/java/it/polimi/ingsw/model/CloudTest.java
+++ src/test/java/it/polimi/ingsw/model/CloudTest.java
@@ -6,29 +6,59 @@ import it.polimi.ingsw.model.entity.*;
 import it.polimi.ingsw.model.enumeration.PawnColor;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Christian Confalonieri
+ */
 public class CloudTest {
 
     private final Cloud myCloud1 = new Cloud();
 
+    /**
+     * @author Christian Confalonieri
+     */
     @Test
     void addStudent() {
 
-        myCloud1.addStudent(new Student(PawnColor.RED));
-        myCloud1.addStudent(new Student(PawnColor.BLUE));
-        myCloud1.addStudent(new Student(PawnColor.YELLOW));
+        Student student1 = new Student(PawnColor.RED);
+        Student student2 = new Student(PawnColor.BLUE);
+        Student student3 = new Student(PawnColor.YELLOW);
+        List<Student> students = new ArrayList<>();
+        students.add(student1);
+        students.add(student2);
+        students.add(student3);
+
+        myCloud1.addStudent(student1);
+        myCloud1.addStudent(student2);
+        myCloud1.addStudent(student3);
 
         assertEquals(3, myCloud1.getStudents().size());
+        assertTrue(myCloud1.getStudents().containsAll(students));
 
     }
 
+    /**
+     * @author Christian Confalonieri
+     */
     @Test
     void pickAllStudents() {
 
-        myCloud1.addStudent(new Student(PawnColor.RED));
-        myCloud1.addStudent(new Student(PawnColor.BLUE));
-        myCloud1.addStudent(new Student(PawnColor.YELLOW));
+        Student student1 = new Student(PawnColor.RED);
+        Student student2 = new Student(PawnColor.BLUE);
+        Student student3 = new Student(PawnColor.YELLOW);
+        List<Student> students = new ArrayList<>();
+        students.add(student1);
+        students.add(student2);
+        students.add(student3);
 
-        assertEquals(3, myCloud1.getStudents().size());
+        myCloud1.addStudent(student1);
+        myCloud1.addStudent(student2);
+        myCloud1.addStudent(student3);
+
+        assertEquals(students,myCloud1.pickAllStudents());
+        assertTrue(myCloud1.getStudents().isEmpty());
 
     }
 
