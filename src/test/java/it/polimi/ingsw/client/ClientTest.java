715c98356b2448dd188096893f5677e79fff8c41
Christian Confalonieri <100704012+christian-confalonieri@users.noreply.github.com>
2022-04-22 11:55:55 +0200

    PlayCardRequest Test

--- src/test/java/it/polimi/ingsw/client/ClientTest.java
+++ src/test/java/it/polimi/ingsw/client/ClientTest.java
@@ -1,10 +1,13 @@
 package it.polimi.ingsw.client;
 
+import it.polimi.ingsw.action.PlayCardAction;
+import it.polimi.ingsw.client.controller.services.GameService;
 import it.polimi.ingsw.client.controller.services.LobbyService;
 import it.polimi.ingsw.client.controller.services.LoginService;
 import it.polimi.ingsw.model.entity.Player;
 import it.polimi.ingsw.model.enumeration.Wizard;
 import it.polimi.ingsw.model.game.GameHandler;
+import it.polimi.ingsw.server.Server;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -69,7 +72,7 @@ class ClientTest {
         new Thread(() -> Client.main(args)).start();
         Thread.sleep(1000);
 
-        LoginService.loginRequest("confa");
+        LoginService.loginRequest("Luigi");
         Thread.sleep(1000);
 
         do {
@@ -87,8 +90,59 @@ class ClientTest {
 
         assertEquals(Client.getInstance().getClientState(), ClientState.INGAME);
         assertNotNull(Client.getInstance().getGameHandler());
-        assertTrue(Client.getInstance().getGameHandler().getGame().getPlayers().stream().map(Player::getName).toList().contains("confa"));
-        
+        assertTrue(Client.getInstance().getGameHandler().getGame().getPlayers().stream().map(Player::getName).toList().contains("Luigi"));
     }
 
+    @Test
+    void Client1GameService() throws InterruptedException {
+        String[] args = {};
+        new Thread(() -> Client.main(args)).start();
+        Thread.sleep(1000);
+
+        LoginService.loginRequest("Mario");
+        Thread.sleep(1000);
+
+        LobbyService.newGameRequest(2, null, Wizard.BLUE);
+        Thread.sleep(1000);
+
+        LobbyService.getAllLobbysRequest();
+
+        /* PlayCardRequest Testing ----------------------------------------------------------------------------------------*/
+        for(int i=15; i>0; i--) {
+            System.out.println(i);
+            Thread.sleep(1000);
+        }
+        GameService.playCardRequest(Client.getInstance().getGameHandler().getCurrentPlayer().getHandCards().get(3));
+        Thread.sleep(1000);
+        assertEquals(4,Client.getInstance().getGameHandler().getGame().getPlayers().get(0).getLastPlayedCard().getNumber());
+        assertFalse(Client.getInstance().getGameHandler().getGame().getPlayers().get(0).getHandCards().contains(Client.getInstance().getGameHandler().getGame().getPlayers().get(0).getLastPlayedCard()));
+    }
+
+    @Test
+    void Client2GameService() throws InterruptedException {
+        String[] args = {};
+        new Thread(() -> Client.main(args)).start();
+        Thread.sleep(1000);
+
+        LoginService.loginRequest("Luigi");
+        Thread.sleep(1000);
+
+        do {
+            LobbyService.getAllLobbysRequest();
+            Thread.sleep(100);
+        } while(Client.getInstance().getAllServerLobbys() == null || Client.getInstance().getAllServerLobbys().isEmpty());
+
+        LobbyService.joinGameRequest(Client.getInstance().getAllServerLobbys().get(0).getGameLobbyId(), Wizard.GREEN);
+        Thread.sleep(1000);
+
+        /* PlayCardRequest Testing ----------------------------------------------------------------------------------------*/
+        for(int i=15; i>0; i--) {
+            System.out.println(i);
+            Thread.sleep(1000);
+        }
+        GameService.playCardRequest(Client.getInstance().getGameHandler().getCurrentPlayer().getHandCards().get(2));
+        Thread.sleep(1000);
+        assertEquals(3,Client.getInstance().getGameHandler().getGame().getPlayers().get(1).getLastPlayedCard().getNumber());
+        assertFalse(Client.getInstance().getGameHandler().getGame().getPlayers().get(1).getHandCards().contains(Client.getInstance().getGameHandler().getGame().getPlayers().get(1).getLastPlayedCard()));
+    }
 }
