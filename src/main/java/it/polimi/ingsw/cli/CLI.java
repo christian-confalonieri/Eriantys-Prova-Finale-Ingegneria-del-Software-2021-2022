cf392d8e5edf4b6b7b677e6bb9f487120912544f
Leonardo Airoldi <leonardo.airoldi@mail.polimi.it>
2022-05-13 23:47:49 +0200

    CLI ClearScreen ANSI

--- src/main/java/it/polimi/ingsw/cli/CLI.java
+++ src/main/java/it/polimi/ingsw/cli/CLI.java
@@ -82,7 +82,8 @@ public class CLI {
             }
             else
             {
-                Runtime.getRuntime().exec("clear");
+                System.out.print("\033[H\033[2J");
+                System.out.flush();
             }
         }
         catch (final Exception e)
