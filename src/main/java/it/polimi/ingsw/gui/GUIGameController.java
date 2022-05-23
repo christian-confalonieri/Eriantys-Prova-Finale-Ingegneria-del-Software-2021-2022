fc20a8331d401a3f3d735ba13f83cdcaf461e562
Christian Confalonieri <100704012+christian-confalonieri@users.noreply.github.com>
2022-05-23 10:35:05 +0200

    GUI: school graphics

--- src/main/java/it/polimi/ingsw/gui/GUIGameController.java
+++ src/main/java/it/polimi/ingsw/gui/GUIGameController.java
@@ -24,7 +24,7 @@ public class GUIGameController {
      * @author Christian Confalonieri
      */
     protected static void initSceneAndController(Stage stage) {
-        FXMLLoader fxmlLoader = new FXMLLoader(GUIGameController.class.getResource("/it/polimi/ingsw/game-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(GUIGameController.class.getResource("/it/polimi/ingsw/school-view.fxml"));
         Parent root = null;
         try {
             root = fxmlLoader.load();
@@ -35,8 +35,8 @@ public class GUIGameController {
             Client.getInstance().getGui().guiGameController = (GUIGameController) fxmlLoader.getController(); // Loads the controller
             Client.getInstance().getGui().notifyAll(); // Wakes up the future waiting for the controller
         }
-        Scene scene = new Scene(root, 600, 400);
-        stage.setTitle("Game");
+        Scene scene = new Scene(root);
+        stage.setTitle("School");
         stage.setScene(scene);
         stage.show();
     }
