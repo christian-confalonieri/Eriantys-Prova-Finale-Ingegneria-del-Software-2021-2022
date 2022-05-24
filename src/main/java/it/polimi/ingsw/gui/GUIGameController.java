270f57fa3e1cb970fc8ae8247b075d29048ede64
Christian Confalonieri <100704012+christian-confalonieri@users.noreply.github.com>
2022-05-24 13:20:13 +0200

    GUI: school selection methods

--- src/main/java/it/polimi/ingsw/gui/GUIGameController.java
+++ src/main/java/it/polimi/ingsw/gui/GUIGameController.java
@@ -1,6 +1,7 @@
 package it.polimi.ingsw.gui;
 
 import it.polimi.ingsw.client.Client;
+import it.polimi.ingsw.client.controller.services.LoginService;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -97,6 +98,102 @@ public class GUIGameController {
     private ImageView cloud4;
     @FXML
     private ImageView outlineCloud4;
+    @FXML
+    private ImageView assistant1;
+    @FXML
+    private ImageView outlineAssistant1;
+    @FXML
+    private ImageView assistant2;
+    @FXML
+    private ImageView outlineAssistant2;
+    @FXML
+    private ImageView assistant3;
+    @FXML
+    private ImageView outlineAssistant3;
+    @FXML
+    private ImageView assistant4;
+    @FXML
+    private ImageView outlineAssistant4;
+    @FXML
+    private ImageView assistant5;
+    @FXML
+    private ImageView outlineAssistant5;
+    @FXML
+    private ImageView assistant6;
+    @FXML
+    private ImageView outlineAssistant6;
+    @FXML
+    private ImageView assistant7;
+    @FXML
+    private ImageView outlineAssistant7;
+    @FXML
+    private ImageView assistant8;
+    @FXML
+    private ImageView outlineAssistant8;
+    @FXML
+    private ImageView assistant9;
+    @FXML
+    private ImageView outlineAssistant9;
+    @FXML
+    private ImageView assistant10;
+    @FXML
+    private ImageView outlineAssistant10;
+    @FXML
+    private ImageView student1;
+    @FXML
+    private ImageView outlineStudent1;
+    @FXML
+    private ImageView student2;
+    @FXML
+    private ImageView outlineStudent2;
+    @FXML
+    private ImageView student3;
+    @FXML
+    private ImageView outlineStudent3;
+    @FXML
+    private ImageView student4;
+    @FXML
+    private ImageView outlineStudent4;
+    @FXML
+    private ImageView student5;
+    @FXML
+    private ImageView outlineStudent5;
+    @FXML
+    private ImageView student6;
+    @FXML
+    private ImageView outlineStudent6;
+    @FXML
+    private ImageView student7;
+    @FXML
+    private ImageView outlineStudent7;
+    @FXML
+    private ImageView student8;
+    @FXML
+    private ImageView outlineStudent8;
+    @FXML
+    private ImageView student9;
+    @FXML
+    private ImageView outlineStudent9;
+    @FXML
+    private ImageView diningRoomGreen;
+    @FXML
+    private ImageView outlineDiningRoomGreen;
+    @FXML
+    private ImageView diningRoomRed;
+    @FXML
+    private ImageView outlineDiningRoomRed;
+    @FXML
+    private ImageView diningRoomYellow;
+    @FXML
+    private ImageView outlineDiningRoomYellow;
+    @FXML
+    private ImageView diningRoomPink;
+    @FXML
+    private ImageView outlineDiningRoomPink;
+    @FXML
+    private ImageView diningRoomBlue;
+    @FXML
+    private ImageView outlineDiningRoomBlue;
 
     /**
      * @author Christian Confalonieri
@@ -382,5 +479,349 @@ public class GUIGameController {
      */
     public void deselectCloud4() {
         outlineCloud4.setVisible(false);
+    }
+
+    /**
+     * @author Christian Confalonieri
+     */
+    public void selectAssistant1() {
+        outlineAssistant1.setVisible(true);
+    }
+
+    /**
+     * @author Christian Confalonieri
+     */
+    public void deselectAssistant1() {
+        outlineAssistant1.setVisible(false);
+    }
+
+    /**
+     * @author Christian Confalonieri
+     */
+    public void selectAssistant2() {
+        outlineAssistant2.setVisible(true);
+    }
+
+    /**
+     * @author Christian Confalonieri
+     */
+    public void deselectAssistant2() {
+        outlineAssistant2.setVisible(false);
+    }
+
+    /**
+     * @author Christian Confalonieri
+     */
+    public void selectAssistant3() {
+        outlineAssistant3.setVisible(true);
+    }
+
+    /**
+     * @author Christian Confalonieri
+     */
+    public void deselectAssistant3() {
+        outlineAssistant3.setVisible(false);
+    }
+
+    /**
+     * @author Christian Confalonieri
+     */
+    public void selectAssistant4() {
+        outlineAssistant4.setVisible(true);
+    }
+
+    /**
+     * @author Christian Confalonieri
+     */
+    public void deselectAssistant4() {
+        outlineAssistant4.setVisible(false);
+    }
+
+    /**
+     * @author Christian Confalonieri
+     */
+    public void selectAssistant5() {
+        outlineAssistant5.setVisible(true);
+    }
+
+    /**
+     * @author Christian Confalonieri
+     */
+    public void deselectAssistant5() {
+        outlineAssistant5.setVisible(false);
+    }
+
+    /**
+     * @author Christian Confalonieri
+     */
+    public void selectAssistant6() {
+        outlineAssistant6.setVisible(true);
+    }
+
+    /**
+     * @author Christian Confalonieri
+     */
+    public void deselectAssistant6() {
+        outlineAssistant6.setVisible(false);
+    }
+
+    /**
+     * @author Christian Confalonieri
+     */
+    public void selectAssistant7() {
+        outlineAssistant7.setVisible(true);
+    }
+
+    /**
+     * @author Christian Confalonieri
+     */
+    public void deselectAssistant7() {
+        outlineAssistant7.setVisible(false);
+    }
+
+    /**
+     * @author Christian Confalonieri
+     */
+    public void selectAssistant8() {
+        outlineAssistant8.setVisible(true);
+    }
+
+    /**
+     * @author Christian Confalonieri
+     */
+    public void deselectAssistant8() {
+        outlineAssistant8.setVisible(false);
+    }
+
+    /**
+     * @author Christian Confalonieri
+     */
+    public void selectAssistant9() {
+        outlineAssistant9.setVisible(true);
+    }
+
+    /**
+     * @author Christian Confalonieri
+     */
+    public void deselectAssistant9() {
+        outlineAssistant9.setVisible(false);
+    }
+
+    /**
+     * @author Christian Confalonieri
+     */
+    public void selectAssistant10() {
+        outlineAssistant10.setVisible(true);
+    }
+
+    /**
+     * @author Christian Confalonieri
+     */
+    public void deselectAssistant10() {
+        outlineAssistant10.setVisible(false);
+    }
+
+    /**
+     * @author Christian Confalonieri
+     */
+    public void selectStudent1() {
+        outlineStudent1.setVisible(true);
+    }
+
+    /**
+     * @author Christian Confalonieri
+     */
+    public void deselectStudent1() {
+        outlineStudent1.setVisible(false);
+    }
+
+    /**
+     * @author Christian Confalonieri
+     */
+    public void selectStudent2() {
+        outlineStudent2.setVisible(true);
+    }
+
+    /**
+     * @author Christian Confalonieri
+     */
+    public void deselectStudent2() {
+        outlineStudent2.setVisible(false);
+    }
+
+    /**
+     * @author Christian Confalonieri
+     */
+    public void selectStudent3() {
+        outlineStudent3.setVisible(true);
+    }
+
+    /**
+     * @author Christian Confalonieri
+     */
+    public void deselectStudent3() {
+        outlineStudent3.setVisible(false);
+    }
+
+    /**
+     * @author Christian Confalonieri
+     */
+    public void selectStudent4() {
+        outlineStudent4.setVisible(true);
+    }
+
+    /**
+     * @author Christian Confalonieri
+     */
+    public void deselectStudent4() {
+        outlineStudent4.setVisible(false);
+    }
+
+    /**
+     * @author Christian Confalonieri
+     */
+    public void selectStudent5() {
+        outlineStudent5.setVisible(true);
+    }
+
+    /**
+     * @author Christian Confalonieri
+     */
+    public void deselectStudent5() {
+        outlineStudent5.setVisible(false);
+    }
+
+    /**
+     * @author Christian Confalonieri
+     */
+    public void selectStudent6() {
+        outlineStudent6.setVisible(true);
+    }
+
+    /**
+     * @author Christian Confalonieri
+     */
+    public void deselectStudent6() {
+        outlineStudent6.setVisible(false);
+    }
+
+    /**
+     * @author Christian Confalonieri
+     */
+    public void selectStudent7() {
+        outlineStudent7.setVisible(true);
+    }
+
+    /**
+     * @author Christian Confalonieri
+     */
+    public void deselectStudent7() {
+        outlineStudent7.setVisible(false);
+    }
+
+    /**
+     * @author Christian Confalonieri
+     */
+    public void selectStudent8() {
+        outlineStudent8.setVisible(true);
+    }
+
+    /**
+     * @author Christian Confalonieri
+     */
+    public void deselectStudent8() {
+        outlineStudent8.setVisible(false);
+    }
+
+    /**
+     * @author Christian Confalonieri
+     */
+    public void selectStudent9() {
+        outlineStudent9.setVisible(true);
+    }
+
+    /**
+     * @author Christian Confalonieri
+     */
+    public void deselectStudent9() {
+        outlineStudent9.setVisible(false);
+    }
+
+    /**
+     * @author Christian Confalonieri
+     */
+    public void selectDiningRoomGreen() {
+        outlineDiningRoomGreen.setVisible(true);
+    }
+
+    /**
+     * @author Christian Confalonieri
+     */
+    public void deselectDiningRoomGreen() {
+        outlineDiningRoomGreen.setVisible(false);
+    }
+
+    /**
+     * @author Christian Confalonieri
+     */
+    public void selectDiningRoomRed() {
+        outlineDiningRoomRed.setVisible(true);
+    }
+
+    /**
+     * @author Christian Confalonieri
+     */
+    public void deselectDiningRoomRed() {
+        outlineDiningRoomRed.setVisible(false);
+    }
+
+    /**
+     * @author Christian Confalonieri
+     */
+    public void selectDiningRoomYellow() {
+        outlineDiningRoomYellow.setVisible(true);
+    }
+
+    /**
+     * @author Christian Confalonieri
+     */
+    public void deselectDiningRoomYellow() {
+        outlineDiningRoomYellow.setVisible(false);
+    }
+
+    /**
+     * @author Christian Confalonieri
+     */
+    public void selectDiningRoomPink() {
+        outlineDiningRoomPink.setVisible(true);
+    }
+
+    /**
+     * @author Christian Confalonieri
+     */
+    public void deselectDiningRoomPink() {
+        outlineDiningRoomPink.setVisible(false);
+    }
+
+    /**
+     * @author Christian Confalonieri
+     */
+    public void selectDiningRoomBlue() {
+        outlineDiningRoomBlue.setVisible(true);
+    }
+
+    /**
+     * @author Christian Confalonieri
+     */
+    public void deselectDiningRoomBlue() {
+        outlineDiningRoomBlue.setVisible(false);
+    }
+
+    /**
+     * @author Christian Confalonieri
+     */
+    @FXML
+    public void logout() {
+        LoginService.logoutRequest();
     }
 }
