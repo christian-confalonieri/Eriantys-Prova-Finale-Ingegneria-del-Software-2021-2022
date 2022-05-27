fba9057faa3512c67cc40a634674ffcbbdc776f0
Leonardo Airoldi <leonardo.airoldi@mail.polimi.it>
2022-05-27 19:40:54 +0200

    School Lanes rendering

--- src/main/java/it/polimi/ingsw/gui/GUISchoolController.java
+++ src/main/java/it/polimi/ingsw/gui/GUISchoolController.java
@@ -1,7 +1,11 @@
 package it.polimi.ingsw.gui;
 
+import it.polimi.ingsw.client.Client;
 import it.polimi.ingsw.model.entity.Player;
+import it.polimi.ingsw.model.enumeration.PawnColor;
 import javafx.fxml.FXML;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
 
 public class GUISchoolController {
@@ -23,6 +27,45 @@ public class GUISchoolController {
     }
 
     public void render() {
+        renderLane(greenHBox, PawnColor.GREEN);
+        renderLane(redHBox, PawnColor.RED);
+        renderLane(yellowHBox, PawnColor.YELLOW);
+        renderLane(pinkHBox, PawnColor.PINK);
+        renderLane(blueHBox, PawnColor.BLUE);
 
+    }
+
+    private void renderLane(HBox hBox, PawnColor pawnColor) {
+        int studentsNumber = playerModel.getSchool().getStudentsDiningRoom(pawnColor).size();
+        double height = hBox.getHeight();
+
+        int inLaneStudents = hBox.getChildren().size();
+
+        if(inLaneStudents == studentsNumber) return;
+        if(inLaneStudents < studentsNumber) {
+            while(inLaneStudents < studentsNumber) {
+                hBox.getChildren().add(getStudentImage(pawnColor, height));
+                inLaneStudents++;
+            }
+        }
+        else {
+            while(inLaneStudents > studentsNumber) {
+                hBox.getChildren().remove(inLaneStudents - 1);
+                inLaneStudents--;
+            }
+        }
+    }
+
+    private ImageView getStudentImage(PawnColor color, double height) {
+        ImageView imageView = new ImageView(switch(color) {
+            case GREEN -> new Image(this.getClass().getResource("/assets/students/green.png").toExternalForm());
+            case RED -> new Image(this.getClass().getResource("/assets/students/red.png").toExternalForm());
+            case PINK -> new Image(this.getClass().getResource("/assets/students/pink.png").toExternalForm());
+            case BLUE -> new Image(this.getClass().getResource("/assets/students/blue.png").toExternalForm());
+            case YELLOW -> new Image(this.getClass().getResource("/assets/students/yellow.png").toExternalForm());
+        });
+        imageView.setPreserveRatio(true);
+        imageView.setFitHeight(height);
+        return imageView;
     }
 }
