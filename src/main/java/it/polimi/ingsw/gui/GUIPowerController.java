abebc57990e1939fefe04dbdefc9be0daaf30988
Leonardo Airoldi <leonardo.airoldi@mail.polimi.it>
2022-05-27 17:03:08 +0200

    GUI Table Powers

--- src/main/java/it/polimi/ingsw/gui/GUIPowerController.java
+++ src/main/java/it/polimi/ingsw/gui/GUIPowerController.java
@@ -1,4 +1,43 @@
 package it.polimi.ingsw.gui;
 
+import it.polimi.ingsw.model.power.PowerCard;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+
 public class GUIPowerController {
+    @FXML
+    private Label cost;
+    @FXML
+    private AnchorPane anchorPane;
+    @FXML
+    private ImageView powerCard;
+
+    private PowerCard powerCardModel;
+
+    public void setPowerCard(PowerCard powerCard) {
+        this.powerCardModel = powerCard;
+        switch(powerCard.getType()) {
+            case HERBALIST -> this.powerCard.setImage(new Image(this.getClass().getResource("/assets/characterCards/edited/herbalist_edited.png").toExternalForm()));
+            case FARMER -> this.powerCard.setImage(new Image(this.getClass().getResource("/assets/characterCards/edited/farmer_edited.png").toExternalForm()));
+            case CENTAUR -> this.powerCard.setImage(new Image(this.getClass().getResource("/assets/characterCards/edited/centaur_edited.png").toExternalForm()));
+            case FRIAR -> this.powerCard.setImage(new Image(this.getClass().getResource("/assets/characterCards/edited/friar_edited.png").toExternalForm()));
+            case HARVESTER -> this.powerCard.setImage(new Image(this.getClass().getResource("/assets/characterCards/edited/harvester_edited.png").toExternalForm()));
+            case HERALD -> this.powerCard.setImage(new Image(this.getClass().getResource("/assets/characterCards/edited/herald_edited.png").toExternalForm()));
+            case JESTER -> this.powerCard.setImage(new Image(this.getClass().getResource("/assets/characterCards/edited/jester_edited.png").toExternalForm()));
+            case KNIGHT -> this.powerCard.setImage(new Image(this.getClass().getResource("/assets/characterCards/edited/knight.png").toExternalForm()));
+            case MAILMAN -> this.powerCard.setImage(new Image(this.getClass().getResource("/assets/characterCards/edited/mailman_edited.png").toExternalForm()));
+            case MINSTREL -> this.powerCard.setImage(new Image(this.getClass().getResource("/assets/characterCards/edited/minstrel_edited.png").toExternalForm()));
+            case PRINCESS -> this.powerCard.setImage(new Image(this.getClass().getResource("/assets/characterCards/edited/princess_edited.png").toExternalForm()));
+            case THIEF -> this.powerCard.setImage(new Image(this.getClass().getResource("/assets/characterCards/edited/thief_edited.png").toExternalForm()));
+        }
+    }
+
+    public void render() {
+        cost.setText(String.valueOf(powerCardModel.getCost()));
+    }
+
 }
+
--- src/main/java/it/polimi/ingsw/gui/GUITableController.java
+++ src/main/java/it/polimi/ingsw/gui/GUITableController.java
@@ -4,6 +4,7 @@ import it.polimi.ingsw.client.Client;
 import it.polimi.ingsw.model.entity.Cloud;
 import it.polimi.ingsw.model.entity.Island;
 import it.polimi.ingsw.model.entity.Player;
+import it.polimi.ingsw.model.power.PowerCard;
 import javafx.fxml.FXML;
 import javafx.scene.layout.AnchorPane;
 import java.util.Arrays;
@@ -46,6 +47,17 @@ public class GUITableController {
 
     public List<AnchorPane> getPlayerTableAnchorPanes() {
         return playerTableAnchorPanes;
+    }
+
+    private List<GUIPowerController> powerControllers;
+    private List<AnchorPane> powerAnchorPanes;
+
+    public List<GUIPowerController> getPowerControllers() {
+        return powerControllers;
+    }
+
+    public List<AnchorPane> getPowerAnchorPanes() {
+        return powerAnchorPanes;
     }
 
     @FXML
@@ -200,6 +212,15 @@ public class GUITableController {
         playerTableControllers = playerTableControllers.stream().filter(GUIPlayerTableController::isBoundToModel).toList();
         playerTableAnchorPanes = playerTableAnchorPanes.stream().limit(playerTableControllers.size()).toList();
 
+
+        // Bind power card views with the characters in game
+        powerControllers = List.of(power1Controller, power2Controller, power3Controller);
+        powerAnchorPanes = List.of(power1, power2, power3);
+
+        Iterator<GUIPowerController> powerControllerIt = powerControllers.iterator();
+        for (PowerCard powerCard : Client.getInstance().getGameHandler().getGame().getPowerCards()) {
+            powerControllerIt.next().setPowerCard(powerCard);
+        }
     }
 
 
@@ -263,6 +284,9 @@ public class GUITableController {
         int[] playerTableX = new int[] {0, 750, 0, 750};
         int[] playerTableY = new int[] {0, 0, 500, 500};
 
+        int[] powerX = new int[] {810, 810, 810};
+        int[] powerY = new int[] {130, 240, 350};
+
 
         Iterator<Integer> islandsXIt = Arrays.stream(islandsX).iterator();
         Iterator<Integer> islandsYIt = Arrays.stream(islandsY).iterator();
@@ -281,6 +305,11 @@ public class GUITableController {
 
         allPlayerTableExecute((playerTablePane, playerTableController) -> renderPlayerTable(playerTablePane, playerTableController, playerTableXIt.next(), playerTableYIt.next()));
 
+
+        Iterator<Integer> powerXIt = Arrays.stream(powerX).iterator();
+        Iterator<Integer> powerYIt = Arrays.stream(powerY).iterator();
+
+        allPowerExecute(((anchorPane, guiPowerController) -> renderPower(anchorPane, guiPowerController, powerXIt.next(), powerYIt.next())));
     }
 
     private void renderIsland(AnchorPane islandPane, GUIIslandController islandController, double x, double y) {
@@ -310,6 +339,11 @@ public class GUITableController {
         playerTableController.render();
     }
 
+    private void renderPower(AnchorPane powerAnchorPane, GUIPowerController powerController, double x, double y) {
+        powerAnchorPane.setLayoutX(x);
+        powerAnchorPane.setLayoutY(y);
+        powerController.render();
+    }
 
 
 
@@ -341,6 +375,15 @@ public class GUITableController {
 
         while (playerTableAnchorPaneIt.hasNext() && playerTableControllersIt.hasNext()) {
             function.accept(playerTableAnchorPaneIt.next(), playerTableControllersIt.next());
+        }
+    }
+
+    public void allPowerExecute(BiConsumer<AnchorPane, GUIPowerController> function) {
+        Iterator<AnchorPane> powerAnchorPaneIt = powerAnchorPanes.iterator();
+        Iterator<GUIPowerController> powerControllerIt = powerControllers.iterator();
+
+        while (powerAnchorPaneIt.hasNext() && powerControllerIt.hasNext()) {
+            function.accept(powerAnchorPaneIt.next(), powerControllerIt.next());
         }
     }
 
