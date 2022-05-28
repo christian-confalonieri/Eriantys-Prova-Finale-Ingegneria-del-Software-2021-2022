e46f4bf062c8e6b2802b1e8b7d664f36d021c7e0
Christian Confalonieri <100704012+christian-confalonieri@users.noreply.github.com>
2022-05-28 10:08:42 +0200

    GUI: cloud students (And fixed the 3-player mode)

--- src/main/java/it/polimi/ingsw/gui/GUICloudController.java
+++ src/main/java/it/polimi/ingsw/gui/GUICloudController.java
@@ -2,13 +2,15 @@ package it.polimi.ingsw.gui;
 
 import it.polimi.ingsw.client.Client;
 import it.polimi.ingsw.model.entity.Cloud;
+import it.polimi.ingsw.model.entity.Student;
 import javafx.fxml.FXML;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 
-public class GUICloudController {
+import java.util.List;
 
+public class GUICloudController {
     @FXML
     private AnchorPane cloudPane;
     @FXML
@@ -19,6 +21,8 @@ public class GUICloudController {
     private ImageView student2;
     @FXML
     private ImageView student3;
+    @FXML
+    private ImageView student4;
     @FXML
     private ImageView outline;
 
@@ -43,7 +47,7 @@ public class GUICloudController {
 
 
     protected void render() {
-
+        setStudents();
     }
 
     /**
@@ -108,5 +112,53 @@ public class GUICloudController {
      */
     public void unselectCloud() {
         outline.setOpacity(0);
+    }
+
+    /**
+     * @author Christian Confalonieri
+     */
+    @FXML
+    public void setStudents() {
+        clearStudents();
+        List<Student> students = cloudModel.getStudents();
+        for(int i=1; i<= students.size(); i++) {
+            getGUIStudent(i).setImage(getStudentImage(students.get(i-1)));
+            getGUIStudent(i).setOpacity(1);
+        }
+    }
+
+    /**
+     * @author Christian Confalonieri
+     */
+    private ImageView getGUIStudent(int number) {
+        return switch (number) {
+            case 1 -> student1;
+            case 2 -> student2;
+            case 3 -> student3;
+            case 4 -> student4;
+            default -> null;
+        };
+    }
+
+    /**
+     * @author Christian Confalonieri
+     */
+    private Image getStudentImage(Student student) {
+        return switch(student.getColor()) {
+            case GREEN -> new Image(this.getClass().getResource("/assets/students/green.png").toExternalForm());
+            case RED -> new Image(this.getClass().getResource("/assets/students/red.png").toExternalForm());
+            case PINK -> new Image(this.getClass().getResource("/assets/students/pink.png").toExternalForm());
+            case BLUE -> new Image(this.getClass().getResource("/assets/students/blue.png").toExternalForm());
+            case YELLOW -> new Image(this.getClass().getResource("/assets/students/yellow.png").toExternalForm());
+        };
+    }
+
+    /**
+     * @author Christian Confalonieri
+     */
+    private void clearStudents() {
+        for(int i=1; i<=4; i++) {
+            getGUIStudent(i).setOpacity(0);
+        }
     }
 }
