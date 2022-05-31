b418e1b24d1d07ff36eb5d8f66b1691a7fd4070d
christian-confalonieri <100704012+christian-confalonieri@users.noreply.github.com>
2022-05-31 15:32:02 +0200

    GUI: added leaderboard-view

--- src/main/java/it/polimi/ingsw/gui/GUIScoreboardController.java
+++ src/main/java/it/polimi/ingsw/gui/GUIScoreboardController.java
@@ -1,20 +1,32 @@
 package it.polimi.ingsw.gui;
 
 import it.polimi.ingsw.client.Client;
+import it.polimi.ingsw.client.controller.services.LoginService;
 import it.polimi.ingsw.model.entity.Player;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.util.List;
 
 public class GUIScoreboardController {
+
+    @FXML
+    private Label position;
+    @FXML
+    private Label player;
+    @FXML
+    private ImageView wizard;
+    @FXML
+    private ImageView background;
     @FXML
     protected static void initSceneAndController(Stage stage) {
-        FXMLLoader fxmlLoader = new FXMLLoader(GUIScoreboardController.class.getResource("/it/polimi/ingsw/scoreboard-view.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(GUIScoreboardController.class.getResource("/it/polimi/ingsw/leaderboard-view.fxml"));
         Parent root = null;
         try {
             root = fxmlLoader.load();
@@ -25,8 +37,8 @@ public class GUIScoreboardController {
             Client.getInstance().getGui().guiScoreboardController = (GUIScoreboardController) fxmlLoader.getController(); // Loads the controller
             Client.getInstance().getGui().notifyAll(); // Wakes up the future waiting for the controller
         }
-        Scene scene = new Scene(root, 500, 800);
-        stage.setTitle("Loading...");
+        Scene scene = new Scene(root);
+        stage.setTitle("Eriantys");
         stage.setScene(scene);
         stage.show();
 
@@ -35,5 +47,13 @@ public class GUIScoreboardController {
     public void renderScoreBoard(List<String> scoreboardIds) {
         List<Player> playersScoreboard = scoreboardIds.stream().map(id -> Client.getInstance().getGameHandler().getGame().getPlayerFromId(id)).toList();
 
+    }
+
+    /**
+     * @author Christian Confalonieri
+     */
+    @FXML
+    public void logout() {
+        LoginService.logoutRequest();
     }
 }
