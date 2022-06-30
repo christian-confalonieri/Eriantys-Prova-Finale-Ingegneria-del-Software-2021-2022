695fd69accaf94c16329dfe891def97bdc4f3496
Leonardo Airoldi <leonardo.airoldi@mail.polimi.it>
2022-06-30 18:05:02 +0200

    Revert "Farmer fix n2"
    This reverts commit de6ad78b975d60352d7d7889cca3d1c50568739f.

--- src/main/java/it/polimi/ingsw/model/game/Game.java
+++ src/main/java/it/polimi/ingsw/model/game/Game.java
@@ -106,7 +106,7 @@ public class Game {
             {
                 if (p.getSchool().getStudentsNumber(color) > 0) allZero = false;
                 if(getEffectHandler().isFarmerActive() && getEffectHandler().getEffectPlayer().getName().equals(p.getName())) {
-                    if (p.getSchool().getStudentsNumber(color) >= maxPlayerColor.getSchool().getStudentsNumber(color) && p.getSchool().getStudentsNumber(color) != 0) {
+                    if (p.getSchool().getStudentsNumber(color) >= maxPlayerColor.getSchool().getStudentsNumber(color)) {
                         maxPlayerColor = p;
                         allZero = false;
                     }
